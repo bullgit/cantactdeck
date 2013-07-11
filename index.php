6461c8bc943c4c3b1323c9ce36749c6a6cab2e3f
Tim Pietrusky <timpietrusky@googlemail.com>
2013-07-11 10:58:20 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CantactDeck.com | NSA-free — powered by NSA!</title>
+    <meta charset="utf-8">
+    <meta name="robots" content="index,follow">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="All cantact streams cambined... even more cantacts!">
+    
+    <meta property="og:url" content="http://cantactdeck.com/" />
+    <meta property="og:title" content="CantactDeck.com | NSA-free — powered by NSA!" />
+    <meta property="og:image" content="http://cantactdeck.me/img/cantact_200.jpg" />
+    <meta property="og:description" content="All cantact streams cambined... even more cantacts!" />
+    
+    <link rel="icon" href="img/cantact.ico" type="image/x-icon" />
+    
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<iframe src="http://kantakte.de"></iframe>
+<iframe src="http://cantact.me"></iframe>
+<iframe src="http://cantact.fr"></iframe>
+    
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-42372739-2', 'cantactdeck.com');
+  ga('send', 'pageview');
+</script>
+
+</body>
+</html>
+
